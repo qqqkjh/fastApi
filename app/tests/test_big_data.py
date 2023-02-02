d109c0f54f07ed1e35b464262102a74860fa7ea5
지훈 김 <jhgim@idino.co.kr>
2023-02-02 11:45:55 +0900

    test : 테스트 데이터 입력

--- app/tests/test_big_data.py
+++ app/tests/test_big_data.py
@@ -1,43 +1,10 @@
-from typing import Union, List, Any
-from fastapi import APIRouter, Cookie, Header, Depends
-from sqlalchemy.orm import Session
-from app.api.deps import get_db
-import os
-import logging
-
-logger = logging.getLogger(__name__)
-
-router = APIRouter()
-
-# 여기서부터 모델관련 모듈
-
-# 파이선 자체 GUI 관련 모듈
-from tkinter import *
-# 데이터 분석 라이브러리
-import pandas as pd
-
-from sklearn.model_selection import train_test_split
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import OneHotEncoder
-from imblearn.over_sampling import SMOTE
-
-import numpy as np
-
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import confusion_matrix, classification_report
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
-from sklearn.metrics import roc_curve, auc
+from unittest import TestCase
+from pandas import Series, DataFrame
 
 
-from sklearn.model_selection import cross_validate,cross_val_score # 교차타당도
-from sklearn.pipeline import make_pipeline # 파이프라인 구축
-from sklearn.model_selection import learning_curve, validation_curve # 학습곡선, 검증곡선
-from sklearn.model_selection import GridSearchCV # 하이퍼파라미터 튜닝
-
-
-example_data = [
+class Test(TestCase):
+    def test_df(self):
+        example_data = [
                 {
                     "학번": 'test-1',
                     "학적1": 5,
@@ -383,7 +350,6 @@ example_data = [
                 }
         ]
 
-@router.get("/v1/test")
-async def test():
+        df = DataFrame(example_data);
 
-    return {"settings": 'now'}
+        print(df)
